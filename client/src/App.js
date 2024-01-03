5af7b306f4729efbde37142faa959dea0a3a552d
Premchand <premchandmali001@gmail.com>
2024-01-03 15:16:52 +0530

    all new client added

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+// import { useEffect, useState } from 'react';
 import './App.css';
-
+import Login from './Components/Login';
+import SignUp from './Components/SignUp';
+import ToDoList from './Components/ToDoList';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login />}></Route>
+          <Route path='/todolist' element={<ToDoList/>}></Route>
+          <Route path='/signup' element={<SignUp/>}></Route>
+        </Routes>
+      </BrowserRouter>
+     
+      
     </div>
   );
 }
